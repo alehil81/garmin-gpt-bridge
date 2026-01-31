6824531ffb44a867adf8b82d7f228ff062900566
Amanjot Lehil <aj@Amanjots-MacBook-Pro.local>
2026-01-30 17:44:16 -0800

    Fix debug_sleep garmin client import

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import inspect
 from . import garmin_client
 from .auth import require_bearer_token
 from .models import ActivitiesResponse, WellnessResponse, DailySummaryResponse
-from .garmin_client import fetch_activities, fetch_wellness
+from .garmin_client import fetch_activities, fetch_wellness, _get_garmin_client
 
 app = FastAPI(title="Garmin GPT Bridge", version="1.0.0")
 
